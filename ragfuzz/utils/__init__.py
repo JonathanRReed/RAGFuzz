b28827bd81e2df9afabfedd9cecaebd4b89535d6
Jonathan R. Reed <Contact@JonathanRReed.com>
2025-12-24 15:17:47 -0600

    refactor: simplify `tomllib` import and use `importlib.util` for optional `VRAMMonitor` loading.

--- ragfuzz/utils/__init__.py
+++ ragfuzz/utils/__init__.py
@@ -1,5 +1,6 @@
 """Utility modules."""
 
+import importlib.util
 from typing import Any
 
 from .async_client import close_client, get_async_client
@@ -8,9 +9,8 @@ __all__ = ["close_client", "get_async_client"]
 
 VRAMMonitor: Any = None
 
-try:
-    from .vram import VRAMMonitor
+if importlib.util.find_spec(".vram", __package__):
+    from .vram import VRAMMonitor as _VRAMMonitor
 
+    VRAMMonitor = _VRAMMonitor
     __all__.append("VRAMMonitor")
-except ImportError:
-    pass  # VRAMMonitor remains None
